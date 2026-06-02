1f8e7f2cb300737151cb784d88d798b48a3cb94a
saikiran76 <kskkorada76@gmail.com>
2026-06-02 13:58:40 +0530

    insertion sort added

---
+++ sorting_algos/ins_sort.js
@@ -0,0 +1,14 @@
+function insertionSort(arr) {
+    // Write your logic here
+    const n = arr.length;
+    for(let i = 1; i<n; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while(arr[j] > key && j>=0) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+    
+}
