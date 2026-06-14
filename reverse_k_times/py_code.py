442263f14bf79ccd6caf1aa9f649694e778dc465
saikiran76 <kskkorada76@gmail.com>
2026-06-14 16:07:06 +0530

    revisited k times reversal problem

--- reverse_k_times/py_code.py
+++ reverse_k_times/py_code.py
@@ -1,1 +1,24 @@
 ## For practise, when revising on weekly basis rewrite the code here
+
+class solution:
+    def rev_ktimes(self, arr, k):
+        n = len(arr)
+        k %= n
+
+        def rev(l, r):
+            while(l < r):
+                arr[l], arr[r] = arr[r], arr[l]
+                l += 1
+                r -= 1
+        
+        rev(0, n-1)
+        rev(0, k-1)
+        rev(k, n-1)
+
+        return arr
+
+
+
+sol_obj = solution()
+
+print(sol_obj.rev_ktimes([1, 2, 3, 4, 5, 6, 7], 3))
